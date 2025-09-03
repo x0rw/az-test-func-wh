39aa299972f6e8b0d80c27619f48ce1603012e02
x0rw <mahdi.svt5@gmail.com>
2025-09-03 17:03:30 +0100

    update func py, add slack webhook

--- function_app.py
+++ function_app.py
@@ -2,26 +2,40 @@ import azure.functions as func
 import datetime
 import json
 import logging
+import urllib
 
+webhook_url = (
+    "https://hooks.slack.com/services/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"
+)
 app = func.FunctionApp()
+
 
 @app.route(route="az_test", auth_level=func.AuthLevel.ANONYMOUS)
 def az_test(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
+    name = req.params.get("name")
     if not name:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name = req_body.get("name")
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        dataj = {"text": f"hello {name}"}
+        req = urllib.request.Request(
+            webhook_url, data=json.dumps(dataj).encode("utf-8")
+        )
+        response = urllib.request.urlopen(req)
+
+        return func.HttpResponse(
+            f"Hello, {name}. This HTTP triggered function executed successfully."
+        )
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200,
         )
+
